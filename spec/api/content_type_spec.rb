4f401ea3f4e239d5e82dc923c5683b7519f129ec
Nicholas Duffy <3457341+duffn@users.noreply.github.com>
2022-06-18 08:15:17 -0600

    Get mixed by headers

--- spec/api/content_type_spec.rb
+++ spec/api/content_type_spec.rb
@@ -31,6 +31,13 @@ describe Acme::API do
       expect(last_response.body).to eq({ data: 'A red brown fox jumped over the road.' }.to_xml)
     end
 
+    it 'returns xml by header' do
+      get '/api/mixed', nil, 'HTTP_ACCEPT' => 'application/xml'
+      expect(last_response.status).to eq(200)
+      expect(last_response.headers['Content-Type']).to eq('application/xml')
+      expect(last_response.body).to eq({ data: 'A red brown fox jumped over the road.' }.to_xml)
+    end
+
     it 'returns text' do
       get '/api/mixed.txt'
       expect(last_response.status).to eq(200)
